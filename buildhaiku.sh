3b0f7b8d3f50df685417f2260d1fd74bddb289f5
Zayn Otley <intuitionamiga@gmail.com>
2022-01-27 15:21:01 +0100

    Edited to get latest config.* from gnu git repo

--- buildhaiku.sh
+++ buildhaiku.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cp /boot/home/src/buildtools/libtool/config.guess .
+wget # https://git.savannah.gnu.org/cgit/config.git/plain/config.guess
+wget # https://git.savannah.gnu.org/cgit/config.git/plain/config.sub
+
 ./configure --with-gcc-version=10.2.0 --target=haiku
 make -j8
 
