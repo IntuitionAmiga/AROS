7e85ba5c6e1c4c3de4dd4cd08e8ae0a84c7d06c1
Zayn Otley <intuitionamiga@gmail.com>
2022-01-30 13:35:03 +0100

    More -fPIC patches for gcc build

--- buildhaiku.sh
+++ buildhaiku.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-wget # https://git.savannah.gnu.org/cgit/config.git/plain/config.guess
-wget # https://git.savannah.gnu.org/cgit/config.git/plain/config.sub
+rm -f config.guess
+rm -f config.sub
+wget https://git.savannah.gnu.org/cgit/config.git/plain/config.guess
+wget https://git.savannah.gnu.org/cgit/config.git/plain/config.sub
 
 ./configure --with-gcc-version=10.2.0 --target=haiku
 make -j8
